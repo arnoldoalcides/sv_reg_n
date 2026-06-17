b99a812f2303a338932fc08e9fa03c69bb554db7
Arnoldo Alcides <arnoldo.alcides@gmail.com>
2026-06-16 20:59:24 -0600

    agregando tests y en signal

--- reg_n.sv
+++ reg_n.sv
@@ -1,11 +1,13 @@
 module reg_n #(parameter WIDTH=8) (
     input logic clk,
     input logic rst,
+    input logic en,
     input logic [WIDTH-1:0] d,
     output logic [WIDTH-1:0] q
 );
     always_ff @(posedge clk) begin
         if (rst) q <= '0;
+        else if (en) q <= d;
         else     q <= d;
     end    
 );
--- tb_reg_n.sv
+++ tb_reg_n.sv
@@ -2,11 +2,23 @@ module tb_reg_n;
     parameter WIDTH_TB = 8;
     logic clk_tb=0;
     logic rst_tb;
+    logic en_tb;
     logic [WIDTH_TB-1:0] d_tb, q_tb;
 
     reg_n #(.WIDTH(WIDTH_TB)) dut (
         .clk(clk_tb),
-        .rst(rst_tb), 
+        .rst(rst_tb),
+        .en(en_tb),
+        .d(d_tb),
+        .q(q_tb)
+    );
+    
+    initial begin
+        forever   #5 clk_tb = ~clk_tb;
+    end
+    
+
+    initial begin
         .d(d_tb),
         .q(q_tb)
     );
@@ -21,13 +33,25 @@ module tb_reg_n;
         $dumpvars(0, tb_reg_n);
 
         //-----
-        
-        rst=1; d_test = 8'hAA; #10;
-        rst=0; #10;
+            rst_tb = 0; 
+            en_tb = 0; 
+            d_tb = '0; 
+            
+            #15 rst_tb = 1;
+
 
         // Test1
 
+            #10
+            d_tb = 8'hAA;
+            en_tb = 1;
+
+
         // Test2
+            #10
+            en_tb = 0;
+            d_tb = 8'h55;
+
 
         // Test3
 
